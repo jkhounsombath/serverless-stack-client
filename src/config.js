26562feeab1ef45f7fcf7d91181f2452c6c49f47
Justin Khounsombath <jkhounsombath@ku.edu>
2020-06-26 11:50:40 -0500

    Setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notesuploadsjkhounso"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "rlo1bsdbrf"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_M3ZN8maSA",
+    APP_CLIENT_ID: "43stjbjbsgj4thma09hqr8ijrv",
+    IDENTITY_POOL_ID: "us-east-2:707b4555-f6f4-4cce-9875-40331654"
+  }
+};
